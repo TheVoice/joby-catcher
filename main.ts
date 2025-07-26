baab8b4812da7e26a88d363506473c27ecd230d9
TheVoice <ras.p.voice@gmail.com>
2025-07-26 21:10:34 +0200

    catch method

--- main.ts
+++ main.ts
@@ -1,21 +1,42 @@
-function open2() {
+function open () {
     servos.P2.setAngle(15)
     pins.servoWritePin(AnalogPin.P8, 90)
 }
-
-function leftTurnStep() {
+function _catch () {
+    open()
+    servos.P0.run(60)
+    servos.P1.run(60)
+    basic.pause(1000)
+    servos.P0.stop()
+    servos.P1.stop()
+    close()
+    basic.pause(5000)
+}
+function leftTurnStep () {
+    servos.P0.run(50)
+    servos.P1.run(-50)
+    basic.pause(10)
+    servos.P0.stop()
+    servos.P1.stop()
+}
+function goForward () {
     servos.P0.run(100)
     servos.P1.run(100)
     basic.pause(100)
     servos.P0.stop()
     servos.P1.stop()
 }
-
-function close() {
-    servos.P2.setAngle(90)
-    pins.servoWritePin(AnalogPin.P8, 15)
+function rightTurnStep () {
+    servos.P0.run(-50)
+    servos.P1.run(50)
+    basic.pause(10)
+    servos.P0.stop()
+    servos.P1.stop()
 }
-
+function close () {
+    servos.P2.setAngle(90)
+    pins.servoWritePin(AnalogPin.P8, 30)
+}
 basic.showString("Hello!")
 basic.showLeds(`
     . # . # .
@@ -24,11 +45,8 @@ basic.showLeds(`
     # # . # #
     . # # # .
     `)
-huskylens.initI2c()
-huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
-huskylens.clearOSD()
 close()
-basic.forever(function on_forever() {
+basic.forever(function () {
     huskylens.request()
     if (huskylens.isLearned(1)) {
         huskylens.writeOSD(convertToText(huskylens.readeBox(1, Content1.xCenter)), 20, 20)
@@ -41,8 +59,19 @@ basic.forever(function on_forever() {
                 . . # . .
                 . . # . .
                 `)
-            open2()
-            leftTurnStep()
+            if (huskylens.readeBox(1, Content1.xCenter) < 140) {
+                leftTurnStep()
+            } else {
+                if (huskylens.readeBox(1, Content1.xCenter) > 180) {
+                    rightTurnStep()
+                } else {
+                    if (huskylens.readeBox(1, Content1.yCenter) < 120) {
+                        goForward()
+                    } else {
+                        _catch()
+                    }
+                }
+            }
         } else {
             basic.showLeds(`
                 . . # . .
@@ -51,9 +80,6 @@ basic.forever(function on_forever() {
                 . # . # .
                 . . # . .
                 `)
-            close()
         }
-        
     }
-    
 })
